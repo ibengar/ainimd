f8c2e55149b4b0a5a67a03c453496b45706c036e
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-14 17:57:44 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -157,7 +157,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    conn.sendButton(m.chat, text.trim(), 'Made with ♡ by Aine', null, [['Donasi', '.donasi'],['Owner', '.owner']], m)
+    conn.sendButton(m.chat, text.trim(), 'Made with ♡ by iBeng', null, [[''],['Owner', '.owner']], m)
     /*conn.sendHydrated(m.chat, text.trim(), 'Ⓟ premium | Ⓛ limit', null, 'https://facebook.com/ribeng2', 'FACEBOOK', '', '', [
       [''],
       [''],
