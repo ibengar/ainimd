4624245717db3ed17aa41249c67a731177504649
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-14 18:44:31 +0800

    Add files via upload

--- plugins/buylimit.js
+++ plugins/buylimit.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, command, args }) => {
     global.db.data.users[m.sender].exp -= xpperlimit * count
     global.db.data.users[m.sender].limit += count
     conn.reply(m.chat, `-${xpperlimit * count} XP\n+ ${count} Limit`, m)
-  } else conn.reply(m.chat, `XP tidak mencukupi untuk membeli ${count} limit`, m)
+  } else conn.reply(m.chat, `XP tidak mencukupi untuk membeli ${count} limit atau nunggu reset`, m)
 }
 handler.help = ['buy <jumlah limit>', 'buyall']
 handler.tags = ['xp']
