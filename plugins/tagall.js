6029d46d4cc4c78ad024a77ba20c285081c7a55c
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-17 17:19:17 +0800

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -7,12 +7,12 @@ let handler = async(m, { conn, text, participants }) => {
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <pesan>']
-handler.tags = ['admin']
+handler.tags = ['group']
 handler.command = /^(tagall)$/i
 
 handler.group = true
 handler.admin = true
-
+handler.premium = true
 module.exports = handler
 
 
