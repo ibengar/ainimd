af886bad11bc7154b4c6e96b20f9310b0e8b58a2
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-17 17:24:44 +0800

    Add files via upload

---
+++ plugins/nsfwloli.js
@@ -0,0 +1,23 @@
+let fetch = require('node-fetch')
+     let handler  = async (m, { conn, args }) => {
+    heum = await require('node-fetch')('https://raw.githubusercontent.com/Caliph71/txt/main/nsfwloli.json').then(v => v.json())
+   conn.sendFile(m.chat, heum[Math.floor(Math.random() * heum.length)], 'Server Error!', 'Lolinya Kaak', m, false)
+}
+handler.help = ['nsfwloli']
+handler.tags = ['sange']
+handler.command = /^nsfwloli$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+;handler.disable = false
+;handler.disable = true
+;
