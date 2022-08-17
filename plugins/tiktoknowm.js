2c03f354b38fab82c1a5e6005a5e7bd7e8c2d5dd
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-17 12:08:52 +0800

    Update tiktoknowm.js

--- plugins/tiktoknowm.js
+++ plugins/tiktoknowm.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('betabotz', '/api/download/tiktok', {
+  let res = await fetch(global.API('xteam', '/dl/tiktok', {
     url: args[0]
   }, 'APIKEY'))
   if (res.status !== 200) throw await res.text()
