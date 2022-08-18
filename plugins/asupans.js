c5b088325909bef2af36b13badabb38e2b2dec6c
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-18 17:32:00 +0800

    Update asupans.js

--- plugins/asupans.js
+++ plugins/asupans.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'https://api.zacros.my.id/asupan/random', 'asupan.mp4', 'nih asupannya kak jangan lupa add fb ku https:facebook.com/ribeng2', m)
+	conn.sendFile(m.chat, 'https://betabotz-api.herokuapp.com/api/asupan/gheayubi?apikey=BetaBotz', 'asupan.mp4', 'nih asupannya kak jangan lupa add fb ku https:facebook.com/ribeng2', m)
 }
-handler.help = ['asupan']
+handler.help = ['asupangheayubi']
 handler.tags = ['premium']
 
-handler.command = /^(asupan)$/i
+handler.command = /^(asupangheayubi)$/i
 handler.premium = true
 handler.register = true
 handler.limit = 5
