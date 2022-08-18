8999af843ea3762387e8cf3f47255b87f046e100
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-19 02:18:42 +0800

    Add files via upload

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -3,22 +3,20 @@ let handler = m => m
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
-  let chat = global.db.data.chats[m.chat]
+  let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
 
   if (chat.antiLink && isGroupLink) {
-    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked from this group byee!`)
-    if (isAdmin) return m.reply('*wah anjir , situ admin gabisa dikik 😢. hehe..*')
-    if (!isBotAdmin) return m.reply('*Bot nya bukan admin, mana bisa ngekik 😐*')
-    let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
-    let isLinkconnGc = new RegExp(linkGC, 'i')
-    let isgclink = isLinkconnGc.test(m.text)
-    if (isgclink) return m.reply('*「 ANTI LINK 」*\n\nItu Dilarang❗, Maaf kamu akan saya kik.\nKarena kirim Link👍')
-    await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+    await m.reply(`*「 ANTI LINK 」*\n\nTerdeteksi *${await this.getName(m.sender)}* kamu telah mengirim link group!\n\nMaaf Kamu akan dikick dari grup ini byee!`)
+    if (isAdmin) return m.reply('*Eh maap kamu admin, kamu gk bakal dikick. hehe..*')
+    if (!isBotAdmin) return m.reply('*Bot bukan admin, mana bisa kick orang _-*')
+    let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
+    let isLinkThisGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkThisGc.test(m.text)
+    if (isgclink) return m.reply('*Lol ngirim link group sendiri :v*')
+    await this.groupRemove(m.chat, [m.sender])
   }
   return true
 }
 
 module.exports = handler
-
-
---
+++ plugins/antispam.js
@@ -0,0 +1,26 @@
+let fs = require('fs')
+let delay = require('delay')
+let handler = async (m, { conn, text }) => {
+conn.mining = conn.mining ? conn.mining : {}
+ if (conn.mining[m.chat]) throw '*Jangan Spam, Coldown 7 detik!!!!*'
+ conn.mining[m.chat] = true 
+ setTimeout(() => { 
+   delete conn.mining[m.chat]
+  }, 5 * 1000)
+}
+handler.help = ['.']
+handler.tags = ['xp']
+handler.customPrefix = /^#/
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
+
+module.exports = handler
