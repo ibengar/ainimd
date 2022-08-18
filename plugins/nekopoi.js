c1b744ba9f4f9a5dacf604f65e8169d52a5c67e5
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-18 17:47:15 +0800

    Update nekopoi.js

--- plugins/nekopoi.js
+++ plugins/nekopoi.js
@@ -1,10 +1,16 @@
-let axios = require("axios");
-let handler = async(m, { conn, text }) => { 
+// xie hua piao piao batman membuka kulkas dan mngambil semangka lalu memberikan mayo di atas semangka
 
-  await m.reply('*[❗] WAIT, Tunggu Sebentar*') 
-    axios.get(`https://mhankbarbar.herokuapp.com/api/nekopoi?url=${teks}&apiKey=N2Ws9kp3KTDYtry5Jjyz`).then((res) =>{ 
-    let hasil = `*Nekopoi Link Tersedia* : ${res.data.judul}\n*Result* : ${res.data.result}\n*Dilihat* : ${res.data.dilihat}\n*Thumbnail* : ${res.data.tumbnail}`
-    
-    conn.sendMessage(id, hasil, MessageType.text)
-    
-  
+let fetch = require('node-fetch')
+let handler = async(m, { conn }) => {
+  let res = await fetch('https://betabotz-api.herokuapp.com/api/nsfw/neko2?apikey=BetaBotz')
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'Nyaa', m)
+}
+handler.help = ['nekopoi2']
+handler.tags = ['nsfw']
+handler.command = /^nekopoi2$/i
+handler.limit = true
+
+module.exports = handler
