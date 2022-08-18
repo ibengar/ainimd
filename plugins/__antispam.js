fe330f994709492f7f011f8c66e3289bbfe108f6
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-19 07:45:13 +0800

    Update __antispam.js

--- plugins/__antispam.js
+++ plugins/__antispam.js
@@ -12,7 +12,7 @@ handler.all = async function (m) {
         this.spam[spaming.jid] = spaming
     } else try {
         this.spam[m.sender].spam += 1
-        if (new Date - this.spam[m.sender].lastspam > 5000) {
+        if (new Date - this.spam[m.sender].lastspam > 20) {
             if (this.spam[m.sender].spam > 20) {
                 this.spam[m.sender].spam = 0
                 this.spam[m.sender].lastspam = new Date * 1
