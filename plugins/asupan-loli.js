8bf36044c86291d6a399c365ccf7ea148112c8a6
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-18 17:27:44 +0800

    Update asupan-loli.js

--- plugins/asupan-loli.js
+++ plugins/asupan-loli.js
@@ -1,9 +1,9 @@
 let handler = async(m,{text, conn}) => {
-let supa = 'https://api.zacros.my.id/asupan/loli'
+let supa = 'https://betabotz-api.herokuapp.com/api/asupan/bocil?apikey=BetaBotz'
 conn.sendFile(m.chat, supa, null, 'Nih', m)
 }
-handler.help = ['asupanloli']
+handler.help = ['asupanbocil']
 handler.tags = ['premium']
-handler.command = /^(asupanloli)$/i
+handler.command = /^(asupanbocil)$/i
 
 module.exports = handler
