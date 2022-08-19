d1dc28e451166d797ae5687534488998b9400c71
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-19 17:56:52 +0800

    Update cecanjap.js

--- plugins/cecanjap.js
+++ plugins/cecanjap.js
@@ -1,10 +1,26 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/japan'), 'Nih  ', wm, 'NEXT', '.cevanjapan', m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('Sedang Diproses...')
+  let res = `https://betabotz-api.herokuapp.com/api/cecan/japan?apikey=BetaBotz`
+  conn.sendFile(m.chat, res, 'bdsm.jpg', `wangy wangy wangy`, m, false)
 }
-
-handler.help = ['cecanjapan']
+handler.help = ['cecanjapan'].map(v => v + ' ')
 handler.tags = ['premium']
+
 handler.command = /^(cecanjapan)$/i
-handler.limit = 3
+handler.owner = false
+handler.mods = false
+handler.premium = true
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = 1
 
 module.exports = handler
+
