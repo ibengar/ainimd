668efc76317a7306302fc1e6cfc77eeafbc288fd
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-19 17:52:24 +0800

    Update asupan-thailand.js

--- plugins/asupan-thailand.js
+++ plugins/asupan-thailand.js
@@ -1,10 +1,26 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/thailand'), 'Nih Jgn lupa add fb gw https://facebook.com/ribeng2', wm, 'NEXT', '.cecanth', m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('Sedang Diproses...')
+  let res = `https://betabotz-api.herokuapp.com/api/cecan/thailand?apikey=BetaBotz`
+  conn.sendFile(m.chat, res, 'bdsm.jpg', `wangy wangy wangy`, m, false)
 }
-
-handler.help = ['cecanth']
+handler.help = ['cecanth'].map(v => v + ' ')
 handler.tags = ['premium']
+
 handler.command = /^(cecanth)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
 handler.limit = 1
+
 module.exports = handler
 
