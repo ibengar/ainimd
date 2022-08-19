48b0d8ffe5c4290f5372048d0a2a66dc4eb35c01
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-19 08:21:04 +0800

    Update milf.js

--- plugins/milf.js
+++ plugins/milf.js
@@ -14,7 +14,7 @@ handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
-handler.private = false
+handler.limit = 10
 
 handler.admin = false
 handler.botAdmin = false
