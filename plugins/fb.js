3decd31b276e522115aa72b07659184f01611ee1
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-20 03:11:05 +0800

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -6,8 +6,8 @@ const {
 let handler = async (m, { conn, args, usedPrefix }) => {
 	if (!args[0]) return m.reply('Putting *URL* Facebook..')
     if (!args[0].includes("facebook")) return m.reply(`Url is wrong..\n\n*Example:*\n${usedPrefix}fb https://www.facebook.com/juankcortavarriaoficial/videos/218237676749570/`)
-	// let res = await fetch(`https://api.zacros.my.id/downloader/fbdl/?url=` + args[0])
-	let res = await fetch(`https://api.xteam.xyz/dl/fbv2?apikey=f9f2-4657-e43f&url=` + args[0])
+	// let res = await fetch(`https://masgimenz.my.id/facebook/?url=` + args[0])
+	let res = await fetch(`https://api.violetics.pw/api/downloader/facebook?apikey=f9f2-4657-e43f&url=` + args[0])
 	//if (res.status !== 200) throw `Coba Lagi`
 	let json = await res.json()
 	//if (!json.result) throw `Media tidak ditemukan atau postingan mungkin diprivate`
@@ -37,7 +37,7 @@ const {
 let handler = async (m, { conn, args, usedPrefix }) => {
 	if (!args[0]) return m.reply('Putting *URL* Facebook..')
     if (!args[0].includes("facebook")) return m.reply(`Url is wrong..\n\n*Example:*\n${usedPrefix}fb https://www.facebook.com/juankcortavarriaoficial/videos/218237676749570/`)
-	let res = await fetch(`https://api.zacros.my.id/downloader/fbdl/?url=` + args[0]
+	let res = await fetch(`https://api.zacros.my.id/downloader/fbdl/?url=` + args[0])
 	//if (res.status !== 200) throw `Coba Lagi`
 	let json = await res.json()
 	//if (!json.result) throw `Media tidak ditemukan atau postingan mungkin diprivate`
