bb7e1fcb269cf76dc8a12574e97438ad7402952e
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-19 11:42:04 +0800

    Update boobsanime.js

--- plugins/boobsanime.js
+++ plugins/boobsanime.js
@@ -1,25 +1,26 @@
-let axios = require('axios');
 let fetch = require('node-fetch')
-let neko = require('nekos.life')
-let Neko = new neko()
-     let handler  = async (m, { conn, args }) => {
-if (!DATABASE.data.chats[m.chat].nsfw && m.isGroup) throw 'Feature Nsfw Disable\nType *!enable* *nsfw* to activate this feature'
-     json = (await axios.get('https://meme-api.herokuapp.com/gimme/biganimetiddies')).data
-   conn.sendFile(m.chat, json.url, 'boobs.jpg', '', m, false)
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('Sedang Diproses...')
+  let res = `https://meme-api.herokuapp.com/gimme/biganimetiddies`
+  conn.sendFile(m.chat, res, 'boobs.jpg', `wangy wangy wangy`, m, false)
 }
-handler.help = ['boobsanime']
+handler.help = ['boobsanime'].map(v => v + ' ')
 handler.tags = ['nsfw']
-handler.command = /^boobsanime$/i
+
+handler.command = /^(boobsanime)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = true
 handler.group = false
 handler.private = false
 
-handler.limit = true
 handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
+handler.limit = true
 
 module.exports = handler
+
