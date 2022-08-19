9f4f82b52939cef1e4eb780cf50f9b4afd0176d6
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-19 17:50:43 +0800

    Update asupan-korea.js

--- plugins/asupan-korea.js
+++ plugins/asupan-korea.js
@@ -1,10 +1,26 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/korea'), 'Nih Jgn lupa add fb gw https://facebook.com/ribeng2', wm, 'NEXT', '.cecankr', m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('Sedang Diproses...')
+  let res = `https://betabotz-api.herokuapp.com/api/cecan/korea?apikey=BetaBotz`
+  conn.sendFile(m.chat, res, 'bdsm.jpg', `wangy wangy wangy`, m, false)
 }
-
-handler.help = ['cecankr']
+handler.help = ['bdsm'].map(v => v + ' ')
 handler.tags = ['premium']
-handler.command = /^(cecankr)$/i
-handler.limit = 3
+
+handler.command = /^(bdsm)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
 module.exports = handler
 
