6068f1bde20a7abe3d1f86da971510437e95e327
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-19 18:02:58 +0800

    Update cecanviet.js

--- plugins/cecanviet.js
+++ plugins/cecanviet.js
@@ -1,10 +1,26 @@
-let handler = async (m, { conn }) => {
-    conn.sendButtonImg(m.chat, ('https://api.zacros.my.id/asupan/vietnam'), 'Nih', wm, 'NEXT', '.cecanvietnam', m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('Sedang Diproses...')
+  let res = `https://betabotz-api.herokuapp.com/api/cecan/hijaber?apikey=BetaBotz`
+  conn.sendFile(m.chat, res, 'bdsm.jpg', `wangy wangy wangy`, m, false)
 }
-
-handler.help = ['cecanvietnam']
+handler.help = ['cecanhijab'].map(v => v + ' ')
 handler.tags = ['premium']
-handler.command = /^(cecanvietnam)$/i
-handler.limit = 3
+
+handler.command = /^(cecanhijab)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = 1
 
 module.exports = handler
+
