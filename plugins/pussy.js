69002ab86b2426c769409bf6daa27226456aa8e6
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-19 12:09:22 +0800

    Update pussy.js

--- plugins/pussy.js
+++ plugins/pussy.js
@@ -1,22 +1,26 @@
-let axios = require('axios');
 let fetch = require('node-fetch')
-let neko = require('nekos.life')
-let Neko = new neko()
-     let handler  = async (m, { conn, args }) => {
-if (!DATABASE.data.chats[m.chat].nsfw && m.isGroup) throw 'Feature Nsfw Disable\nType *!enable* *nsfw* to activate this feature'
-     json = (await axios.get('https://meme-api.herokuapp.com/gimme/pussy')).data
-   conn.sendFile(m.chat, json.url, 'pussy.jpg', json.title, m, false)
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ')
+  if (!args) throw 'Masukkan Parameter'
+  m.reply('Sedang Diproses...')
+  let res = `https://betabotz-api.herokuapp.com/api/nsfw/pussy?apikey=BetaBotz`
+  conn.sendFile(m.chat, res, 'ass.jpg', `wangy wangy wangy`, m, false)
 }
-handler.help = ['pussy']
+handler.help = ['pussy'].map(v => v + ' ')
 handler.tags = ['nsfw']
+
 handler.command = /^(pussy)$/i
-handler.group = false
+handler.owner = false
+handler.mods = false
 handler.premium = true
+handler.group = false
 handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
 handler.limit = true
 
 module.exports = handler
-
-
-
 
