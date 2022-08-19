06a0e646056933d372ab3e02938444b17220de6a
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-20 02:22:07 +0800

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
   if (/^https?:\/\/.*(fb.watch|facebook.com)/i.test(m.text)) throw `url salah`
 
-  let res = await fetch(API('neoxr', '/api/download/fb', { url: args[0] }, 'apikey'))
+  let res = await fetch(API('xteam', '/dl/fbv2', { url: args[0] }, 'apikey'))
   if (!res.ok) throw eror
   let json = await res.json()
   if (!json.status) throw json
