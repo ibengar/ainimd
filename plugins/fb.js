79b6460e02be86c5b0eaba911d96b7ab2a9fefb8
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-20 15:28:21 +0800

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,26 +1,51 @@
-//import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
-import fetch from 'node-fetch'
-//import { savefrom } from '@bochilteam/scraper'
-//import cheerio from 'cheerio'
+let fetch = require('node-fetch')
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-try {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/azFEBmFRcy/`
-    // let { result } = await facebookdl(args[0])
-    //if (!args[1]) return conn.sendButton(m.chat, `*${htki} ғᴀᴄᴇʙᴏᴏᴋ ${htka}*`, null, null, [['sᴅ', `.fb ${args[0]} sd`],['ʜᴅ', `.fb ${args[0]} hd`]],m)
-   let res = await fetch(`https://api.xteam.xyz/dl/fbv2?url=${args[0]}&APIKEY=NezukoTachibana281207`)
-    let { result } = await res.json()
-    
-    let json = await res.json()
-    if (!json.status) throw json
-    await m.reply('Sedang di proses..')
-    await conn.sendFile(m.chat, json.data[1].url, json.data[1].url, wm, m)
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let res = await fetch(`https://api.xteam.xyz/dl/fbv2?url=${args[0]}&APIKEY=NezukoTachibana281207`)
+     if (res.status !== 200) {
+    res.text()
+    throw res.status
+  }
+  let json = await res.json()
+  if (!json.result) throw json
+  let { name, author, description, uploadDate, duration, url, isFamilyFriendly, genre, keywords, contentSize, videoQuality, commentCount } = json.result
+  let { name: authorname, url: authorlink } = author || {}
+  let dateConfig = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  }
+  let unknown = '_Unknown_'
+  let none = '_None_'
+  let caption = `
+Konten${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
+Post oleh ${name} (${authorname || ''}) (${authorlink || ''})
+Diposting pada ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
+Size: ${contentSize || unknown}
+Durasi: ${clockString(+ new Date(duration))}
+Genre: ${genre || none}
+Kualitas: ${videoQuality ? videoQuality : unknown}
+
+${description}
+
+Keyword: ${keywords || none}
+`.trim()
+  conn.sendFile(m.chat, url, 'media-fb', caption, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
-handler.command = /^f((b|acebook|)(dl|download)?(er)?(2)?)$/i
-handler.limit = true
-handler.group = true
+handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
 
 module.exports = handler
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+}
