4384717ad12794666cdc7a8af1967a1254e3a395
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-20 20:27:41 +0800

    Update stalker-pptiktok.js

--- plugins/stalker-pptiktok.js
+++ plugins/stalker-pptiktok.js
@@ -1,26 +1,10 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-   response = args.join(' ')
-  if (!args) throw 'Masukkan Parameter'
-  m.reply('Sedang Diproses...')
-  let res = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=cd7227d604d7acca5d19b4cc
-  conn.sendFile(m.chat, res, 'mstrb.jpg', `selebeww`, m, false)
+let handler = async(m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, 'Masukkan Username Tiktok', m)
+let tetete = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=cd7227d604d7acca5d19b4cc`
+conn.sendFile(m.chat, tetete, `Hasil Dari ${text}`, wm, 'Thanks', 'thanks', m) 
 }
-handler.help = ['pptiktok'].map(v => v + ' ')
-handler.tags = ['downloader']
-
+handler.help = ['pptiktok'].map(v => v + ' <username>')
+handler.tags = ['downloader', 'stalk', 'internet']
 handler.command = /^(pptiktok)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.limit = true
 
 module.exports = handler
-
