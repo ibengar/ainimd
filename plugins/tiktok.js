77f25ae2efeba0fbbfe62269cd5bad3b9ccf8177
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-20 19:24:18 +0800

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,4 +1,4 @@
-let axios = require('axios');
+let fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `contoh:\n ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
     let res = await fetch(`https://betabotz-api.herokuapp.com/api/download/tiktok?url=${args[0]}&apikey=BetaBotz`)
