e774677ef91b5917eb0ed9cc957f7f3060e20736
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-20 09:27:33 +0800

    Update xnxx.js

--- plugins/xnxx.js
+++ plugins/xnxx.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
     let res = await fetch(`https://api.zacros.my.id/nsfw/xnxx-search?query=${text}`)
     if (!res.ok) throw await res.text()
     let json = await res.json()
-    let keqing = json.result.map((v, i) => `#${i + 1}. \n*Title:* ${v.title}\n*Info:* ${v.info}\n*Link:* ${v.link}\n==============\n`).join('\n') 
+    //let keqing = json.result.map((v, i) => `#${i + 1}. \n*Title:* ${v.title}\n*Info:* ${v.info}\n*Link:* ${v.link}\n==============\n`).join('\n') 
     if (json.status) m.reply(keqing)
     else throw json
 }
