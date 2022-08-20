859931339d5708b072def9d44545f1036f80717f
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-20 20:14:46 +0800

    Update stalker-pptiktok.js

--- plugins/stalker-pptiktok.js
+++ plugins/stalker-pptiktok.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, 'Masukkan Username Tiktok', m)
 let tetete = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=cd7227d604d7acca5d19b4cc`
-conn.sendButtonImg(m.chat, tetete, `Hasil Dari ${text}`, wm2, 'Thanks', 'thanks', m) 
+conn.sendButtonImg(m.chat, tetete, `Hasil Dari ${text}`, wm, 'Thanks', 'thanks', m) 
 }
 handler.help = ['pptiktok'].map(v => v + ' <username>')
 handler.tags = ['downloader', 'stalk', 'internet']
