795be492c5bf3f65d54b5c0bc8fb7bb9e41c7f8f
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-20 08:23:07 +0800

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,59 +1,21 @@
-/*let fetch = require('node-fetch')
-const {
-    MessageType
-} = require('@adiwajshing/baileys')
-
-let handler = async (m, { conn, args, usedPrefix }) => {
-	if (!args[0]) return m.reply('Putting *URL* Facebook..')
-    if (!args[0].includes("facebook")) return m.reply(`Url is wrong..\n\n*Example:*\n${usedPrefix}fb https://www.facebook.com/juankcortavarriaoficial/videos/218237676749570/`)
-	// let res = await fetch(`https://masgimenz.my.id/facebook/?url=` + args[0])
-	let res = await fetch(`https://api.violetics.pw/api/downloader/facebook?apikey=f9f2-4657-e43f&url=` + args[0])
-	//if (res.status !== 200) throw `Coba Lagi`
-	let json = await res.json()
-	//if (!json.result) throw `Media tidak ditemukan atau postingan mungkin diprivate`
-	let url = json.result.hd.url
-	m.reply('Sedang diproses...')
-	if (url) await conn.sendFile(m.chat, url, 'fb.mp4', author, m)
-	//if (url) await conn.sendMessage(m.chat, url, MessageType.video, {mimetype: 'video/mp4', quoted: m, caption: wm})
-	else m.reply('Link download tidak ditemukan')
-	}
-
-handler.help = ['fb'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
-handler.limit = true
-handler.group = true
-handler.premium = false
-
-module.exports = handler*/
-
 
 let fetch = require('node-fetch')
-const {
-    MessageType
-} = require('@adiwajshing/baileys')
 
-let handler = async (m, { conn, args, usedPrefix }) => {
-	if (!args[0]) return m.reply('Putting *URL* Facebook..')
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+    if (!args[0]) return m.reply('Putting *URL* Facebook..')
     if (!args[0].includes("facebook")) return m.reply(`Url is wrong..\n\n*Example:*\n${usedPrefix}fb https://www.facebook.com/juankcortavarriaoficial/videos/218237676749570/`)
-	let res = await fetch(`https://api.zacros.my.id/downloader/fbdl/?url=` + args[0])
-	//if (res.status !== 200) throw `Coba Lagi`
-	let json = await res.json()
-	//if (!json.result) throw `Media tidak ditemukan atau postingan mungkin diprivate`
-	let url = json.videoUrl
-	m.reply('Sedang diproses...')
-	if (url) await conn.sendFile(m.chat, url, 'fb.mp4', wm, m)
-	//if (url) await conn.sendMessage(m.chat, url, MessageType.video, {mimetype: 'video/mp4', quoted: m, caption: wm})
-	else m.reply('Link download tidak ditemukan')
-	}
-
+    let res = await fetch(`https://api.xteam.xyz/dl/fbv2/?url=${args[0]}&apikey=HIRO`)
+   
+    let json = await res.json()
+    if (!json.status) throw json
+    await m.reply('Sedang di proses..')
+    await conn.sendFile(m.chat, json.data[1].url, json.data[1].url, wm, m)
+}
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+handler.command = /^f((b|acebook|)(dl|download)?(er)?(1)?)$/i
 handler.limit = true
 handler.group = true
-handler.premium = false
 
 module.exports = handler
