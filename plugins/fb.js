b7222ab43f8a206c5d3ea89a129764889e718241
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-20 09:00:23 +0800

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -1,41 +1,59 @@
+/*const fetch = require('node-fetch')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `Masukkan URL Facebook yang ingin di download!\n\nContoh:${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
+  if (/^https?:\/\/.*(fb.watch|facebook.com)/i.test(m.text)) throw `url salah`
+
+  let res = await fetch(API('neoxr', '/api/download/fb', { url: args[0] }, 'apikey'))
+  if (!res.ok) throw eror
+  let json = await res.json()
+  if (!json.status) throw json
+  await m.reply(wait)
+  await conn.sendFile(m.chat, json.data.sd.url, '', `HD: ${json.data.hd.url}\nUkuran: ${json.data.hd.size}\n\n${watermark}`, m)
+}
+handler.help = ['fb'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+
+handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+
+handler.limit = true
+
+module.exports = handler*/
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, args }) => {
   if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/fbv2', {
-    url: args[0]
-  }, 'APIKEY'))
+  let res = await fetch(global.API('neoxr', '/api/fb', { url: args[0] }, 'apikey'))
   if (res.status !== 200) {
     res.text()
     throw res.status
   }
   let json = await res.json()
   if (!json.result) throw json
-  //let { name, author, description, uploadDate, duration, url, isFamilyFriendly, genre, keywords, contentSize, videoQuality, commentCount } = json.result
-  //let { name: authorname, url: authorlink } = author || {}
-  //let dateConfig = {
-    //hour: 'numeric',
-    //minute: 'numeric',
-    //second: 'numeric',
-    //day: 'numeric',
-    //month: 'long',
-   // year: 'numeric'
+  let { name, author, description, uploadDate, duration, url, isFamilyFriendly, genre, keywords, contentSize, videoQuality, commentCount } = json.result
+  let { name: authorname, url: authorlink } = author || {}
+  let dateConfig = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
   }
-  //let unknown = '_Unknown_'
-  //let none = '_None_'
-  //let caption = `
-//Konten${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
-//Post oleh ${name} (${authorname || ''}) (${authorlink || ''})
-//Diposting pada ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
-//Size: ${contentSize || unknown}
-//Durasi: ${clockString(+ new Date(duration))}
-//Genre: ${genre || none}
-//Kualitas: ${videoQuality ? videoQuality : unknown}
+  let unknown = '_Unknown_'
+  let none = '_None_'
+  let caption = `
+Konten${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
+Post oleh ${name} (${authorname || ''}) (${authorlink || ''})
+Diposting pada ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
+Size: ${contentSize || unknown}
+Durasi: ${clockString(+ new Date(duration))}
+Genre: ${genre || none}
+Kualitas: ${videoQuality ? videoQuality : unknown}
 
-//${description}
+${description}
 
-//Keyword: ${keywords || none}
-//`.trim()
+Keyword: ${keywords || none}
+`.trim()
   conn.sendFile(m.chat, url, 'media-fb', caption, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
