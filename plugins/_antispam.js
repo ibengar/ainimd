955869a5bd0d79283d2296ffab9c80c6faa215f8
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-21 23:18:07 +0800

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,32 +1,22 @@
-let handler = m => m
-
-handler.all = async function (m) {
-if (m.key.fromMe) return 
+export async function all(m) {
+    if (!m.message)
+        return
     this.spam = this.spam ? this.spam : {}
-    if (!(m.sender in this.spam)) {
-        let spaming = {
-        jid: await m.sender, 
-        spam: 0,
-        lastspam: 0
-
-        }
-        this.spam[spaming.jid] = spaming
-    } else try {
-        this.spam[m.sender].spam += 1
-        if (new Date - this.spam[m.sender].lastspam > 2) {
-            if (this.spam[m.sender].spam > 1) {
-                this.spam[m.sender].spam = 0
-                this.spam[m.sender].lastspam = new Date * 1
-                //global.DATABASE._data.users[m.sender].banned = true
-                m.isGroup ? m.reply('*Jangan Spam!!*') : m.reply('*Kamu Terdeteksi Spam Bot\nMaka Bot Akan Blokir Nomor kamu!!*').then(v => conn.blockUser(m.chat))
-            } else {
-                this.spam[m.sender].spam = 0
-                this.spam[m.sender].lastspam = new Date * 1
+    if (m.sender in this.spam) {
+        this.spam[m.sender].count++
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 3) {
+            if (this.spam[m.sender].count > 3) {
+                //global.db.data.users[m.sender].banned = true
+                m.reply('*Jangan Spam!!*')
             }
+            this.spam[m.sender].count = 0
+            this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
         }
-    } catch (e) {
-        console.log(e)
     }
+    else
+        this.spam[m.sender] = {
+            jid: m.sender,
+            count: 0,
+            lastspam: 0
+        }
 }
-
-module.exports = handler
