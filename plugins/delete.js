1de861e7d498cce5d37e964e08c243bceb519967
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-21 21:42:13 +0800

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -2,8 +2,8 @@ let handler = function (m) {
     /*if (!m.quoted) throw false
     let { chat, fromMe, isBaileys } = m.quoted
     if (!fromMe) throw false
-    if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'*/
-    conn.sendMessage(chat, { delete: m.quoted.vM.key })
+    if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
+    conn.sendMessage(chat, { delete: m.quoted.vM.key })*/
     if (!m.quoted) throw false
     let { chat, fromMe, id, isBaileys } = m.quoted
     if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
