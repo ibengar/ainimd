a38984c1c49392163bcd0e85a0d78f445a8743d2
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-22 01:02:47 +0800

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -8,6 +8,30 @@ let format = sizeFormatter({
   keepTrailingZeroes: false,
   render: (literal, symbol) => `${literal} ${symbol}B`,
 })
+let handler = async(m, { conn, args, text, usedPrefix: _p }) => {
+ const ftoko = {
+key: {
+			fromMe: false,
+			participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "0@s.whatsapp.net" } : {})
+		},
+		message: {
+			"productMessage": {
+				"product": {
+					"productImage":{
+						"mimetype": "image/jpeg",
+						"jpegThumbnail": fs.readFileSync(`./src/profil.jpg`) //Gambarnye
+					},
+					"title": "tokonya bang", //Kasih namalu 
+					"description": "iBeng BOT", 
+					"currencyCode": "USD",
+					"priceAmount1000": "2000",
+					"retailerId": "Ghost",
+					"productImageCount": 1
+				},
+				    "businessOwnerJid": `0@s.whatsapp.net`
+		}
+	}
+})
 let handler = async (m, { conn }) => {
   const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
   const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
@@ -43,14 +67,14 @@ let handler = async (m, { conn }) => {
   m.reply(`
 Merespon dalam ${speed} millidetik
 
-💬 Status :
+ðŸ’¬ Status :
 - *${groupsIn.length}* Group Chats
 - *${groupsIn.length}* Groups Joined
 - *${groupsIn.length - groupsIn.length}* Groups Left
 - *${chats.length - groupsIn.length}* Personal Chats
 - *${chats.length}* Total Chats
 
-💻 *Server Info* :
+ðŸ’» *Server Info* :
 RAM: ${format(os.totalmem() - os.freemem())} / ${format(os.totalmem())}
 
 _NodeJS Memory Usage_
