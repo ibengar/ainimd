8197c03ba69809d813ecfa6012c3943a556447b8
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-21 21:23:52 +0800

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -7,7 +7,7 @@ let handler = function (m) {
     if (!m.quoted) throw false
     let { chat, fromMe, id, isBaileys } = m.quoted
     if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
-    conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: true, id: m.quoted.id, participant: m.quoted.sender } })
+    conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.quoted.id, participant: m.quoted.sender } })
 
 }
 handler.help = ['del', 'delete']
