a6639975bcc7b8efa9344844c361ecf77b0dee82
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-21 22:56:54 +0800

    Update sayang.js

--- plugins/sayang.js
+++ plugins/sayang.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-let ayang = fs.readFileSync('./mp3/syg.opus')
+let ayang = fs.readFileSync('./lib/syg.opus')
 conn.sendFile(m.chat, ayang, 'sayank', 'yang', m, true)
 //conn.sendMessage(m.chat, ayang, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 }
