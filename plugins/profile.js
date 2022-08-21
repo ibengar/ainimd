186fe2e4c1f57883e2a3a12fa9326cadfdacba99
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-21 09:34:11 +0800

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,34 +1,34 @@
+let fetch = require('node-fetch')
 let PhoneNumber = require('awesome-phonenumber')
-let levelling = require('../lib/levelling')
-let handler = async (m, { conn, usedPrefix }) => {
+let handler = async (m, { conn }) => {
   let pp = './src/avatar_contact.png'
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   try {
-    pp = await conn.getProfilePicture(who)
+    pp = await conn.profilePictureUrl(who, 'image')
   } catch (e) {
 
   } finally {
-    let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
-    let { name, limit, exp, lastclaim, registered, regTime, age, level, role } = global.DATABASE.data.users[who]
-    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+    let { name, premium, level, limit, exp, lastclaim, registered, regTime, age } = global.DATABASE.data.users[m.sender]
     let username = conn.getName(who)
-    let math = max - xp
-    let prem = global.prems.includes(who.split`@`[0])
     let str = `
-🔖 Name: ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\nAbout: ' + about : ''}
-☎️ Number: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-🔗 Link: https://wa.me/${who.split`@`[0]}${registered ? '\nAge: ' + age : ''}
-📍 XP: TOTAL ${exp} (${exp - min} / ${xp}) [${math <= 0 ? `Ready to *${usedPrefix}levelup*` : `${math} XP left to levelup`}]
-📌 Level: ${level}
-☣️ Limit: ${limit}
-🖇️ Registered: ${registered ? 'Yes (' + new Date(regTime) + ')': 'No'}
-👑 Premium: ${prem ? 'Yes' : 'No'}${lastclaim > 0 ? '\nLast Claim: ' + new Date(lastclaim) : ''}
+
+✧───────[ *PROFILE* ]───────✧
+📇 • *Name:* ${username} ${registered ? '(' + name + ') ': ''}
+📧 • *Tag:* @${who.replace(/@.+/, '')}
+📞 • *Number:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+💻 • *Link:* https://wa.me/${who.split`@`[0]}
+${registered ? '🎨 • *Age:* ' + age : ''}
+
+🌟 • *Premium:* ${premium ? "✅" :"❌"}
+📑 • *Registered:* ${registered ? '✅': '❌'}
+⛔ • *Banned:* ❌
+
 `.trim()
     let mentionedJid = [who]
-    conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
+    conn.sendFile(m.chat, await(await require('node-fetch')(pp)).buffer(), pp.jpg, str, m, false, { contextInfo: { mentionedJid }})
   }
 }
 handler.help = ['profile [@user]']
 handler.tags = ['tools']
-handler.command = /^profile$/i
+handler.command = /^profile|pp$/i
 module.exports = handler
