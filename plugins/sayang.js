d1c253c9ee49a992dcab5b4f3b7059a20657e5ee
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-21 20:40:30 +0800

    Create sayang.js

---
+++ plugins/sayang.js
@@ -0,0 +1,18 @@
+let fs = require('fs')
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m) => {
+let ayang = fs.readFileSync('./mp3/syg.opus')
+conn.sendFile(m.chat, ayang, 'sayank', 'yang', m, true)
+//conn.sendMessage(m.chat, ayang, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
+}
+
+handler.customPrefix = /^(sayang)$/i
+handler.command = new RegExp
+
+handler.limit = true
+handler.mods = false 
+handler.premium = false
+handler.group = false
+handler.private = false
+
+module.exports = handler
