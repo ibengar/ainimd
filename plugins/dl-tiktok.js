9d50dc082cbf2bd1ccc2fb3c33239c32bb0b993d
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-23 15:24:00 +0800

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -18,7 +18,7 @@ await conn.reply(m.chat, `Downloading media from Tiktok`, 0, {
      }}
   })
 let txt = `🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${args[0]}`)).data}` 
-    await conn.send2ButtonVid(m.chat, `https://api.lolhuman.xyz/api/tiktok3?apikey=cd7227d604d7acca5d19b4cc&url=${args[0]}` , txt, wm, `No Wm`, `.tiktoknowm ${args[0]}`, `Audio`, `.tiktokaudio ${args[0]}`, m)
+    await conn.send2ButtonVid(m.chat, `https://api.lolhuman.xyz/api/tiktok3?apikey=cd7227d604d7acca5d19b4cc&url=${args[0]}` , txt, wm, `No Wm`, `.tiktoknowm ${args[0]}`,m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
