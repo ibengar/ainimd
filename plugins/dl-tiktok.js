faac54f524f9afa35a0f7db3ec2fcbf5ed7f3699
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-24 22:06:59 +0800

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
 	if (!args[0].match(/tiktok/gi)) throw `url salah`
 	try {
-		let a = await axios.get('https://rest-beni.herokuapp.com/api/tiktok?url=' + args[0])
+		let res = (await axios.get('https://rest-beni.herokuapp.com/api/tiktok?url=' + args[0])
 		if (!a.data.result.video_original) {
 			conn.sendFile(m.chat, a.data.result.video, '', '\n\nBOTSTYLEE', m)
 		} else {
