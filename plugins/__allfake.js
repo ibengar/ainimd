6984fc78c3242089fa9ff7bc1a619e1b4419b0db
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-24 20:28:57 +0800

    Update __allfake.js

--- plugins/__allfake.js
+++ plugins/__allfake.js
@@ -34,8 +34,8 @@ handler.all = async function (m) {
 		// externalAdReply atau text with thumbnail. gatau bahasa Inggris? coba translate!
 		global.adReply = {
 			contextInfo: {
-				forwardingScore: 9999,
-				isForwarded: true, // ini biar ada tulisannya diteruskan berkali-kali, jika ingin di hilangkan ganti true menjadi false
+				//forwardingScore: 9999,
+				showAdAttribution: true, // ini biar ada tulisannya diteruskan berkali-kali, jika ingin di hilangkan ganti true menjadi false
 				externalAdReply: { // Bagian ini sesuka kalian berkreasi :'v
 					title: "aktif selama: " + global.u,
 					body: wm,
