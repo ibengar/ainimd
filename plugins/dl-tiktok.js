5e5c1862127a54241cc2dc3e27c27070ce76ce00
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-24 21:59:29 +0800

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,13 +1,9 @@
-var handler = async (m, {
-	conn,
-	args,
-	usedPrefix,
-	command
-}) => {
+let axios = require('axios');
+let handler = async (m, { conn, args, usedPrefix, command }) => {
 	if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://vt.tiktok.com/yqyjPX/`
 	if (!args[0].match(/tiktok/gi)) throw `url salah`
 	try {
-		var a = await axios.get('https://rest-beni.herokuapp.com/api/tiktok?url=' + args[0])
+		let a = await axios.get('https://rest-beni.herokuapp.com/api/tiktok?url=' + args[0])
 		if (!a.data.result.video_original) {
 			conn.sendFile(m.chat, a.data.result.video, '', '\n\nBOTSTYLEE', m)
 		} else {
