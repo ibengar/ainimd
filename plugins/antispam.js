b75e6d8053696c93e4fb6e60841f7839165faa21
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-25 14:34:21 +0800

    Update antispam.js

--- plugins/antispam.js
+++ plugins/antispam.js
@@ -6,7 +6,7 @@ conn.mining = conn.mining ? conn.mining : {}
  conn.mining[m.chat] = true 
  setTimeout(() => { 
    delete conn.mining[m.chat]
-  }, 7 * 3)
+  }, 5 * 5)
 }
 handler.help = ['.']
 handler.tags = ['xp']
