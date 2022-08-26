44b9ea51bd53aeada746ab74e52cb5a7d17d3779
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-27 03:26:28 +0800

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,24 +1,23 @@
-let handler = m => m
-
 let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
 
-handler.before = function (m, { isOwner, isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe && isOwner) return !0
-    let chat = db.data.chats[m.chat]
-    let user = db.data.users[m.sender]
+export function before(m, { isBotAdmin }) {
+    if (m.isBaileys && m.fromMe) return !0
+    let chat = global.db.data.chats[m.chat]
+    let user = global.db.data.users[m.sender]
     let isBadword = badwordRegex.exec(m.text)
+    console.log(isBadword)
 
-    if (!chat.antiBadword && !chat.isBanned && !user.banned && isBadword && !isOwner && !isAdmin) {
+    if (chat.antiBadword && isBadword) {
         user.warning += 1
-        this.send2Button(m.chat, `*Badword terdeteksi!*
-Warning: ${user.warning} / 10
-Jika warning mencapai 10 kamu akan *dibanned+kick*
+        this.sendButton(m.chat, `*📮ᴛᴏxɪᴄ ᴛᴇʀᴅᴇᴛᴇᴋꜱɪ !\nhttp://bīt.ly/ᯤ*
+あ Warning: ${user.warning} / 5 ┊
 
-ketik *#on antibadword* untuk menyalakan antibadword
-ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
+[❗] Jika warning mencapai 5 kamu akan *dibanned+kick*
 
-“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, wm, 'MATIKAN ANTIBADWORD', '.off antibadword', 'ASTAGHFIRULLAH', '.maaf', m)
-        if (user.warning >= 10) {
+Anda hanya bisa meminta Owner untuk membuka banned-nya !\n\n🌸 Bot By Fangz
+
+“Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, wm, [['🎀CEK WARN', '.cekdosa'], ['🎐AUTHOR BOT', '.owner']], m)
+        if (user.warning >= 5) {
             user.banned = true
             if (m.isGroup) {
                 if (isBotAdmin) {
@@ -30,4 +29,7 @@ ketik *#astagfirullah* atau *#maaf* untuk mengurangi warning
     }
     return !0
 }
-module.exports = handler
+
+
+
+// jasa buat by Fokusdotid (Fokus ID)
