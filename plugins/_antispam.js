b438f6dcb96973e6a72829b65a34c14b60dc579a
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-27 03:23:17 +0800

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -1,68 +1,22 @@
-let handler = m => m
-module.exports = {
-    async all(m) {
-        if (!m.message) return
-        this.spam = this.spam ? this.spam : {}
-        if (m.sender in this.spam) {
-            this.spam[m.sender].count++
-            if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
-                if (this.spam[m.sender].count > 5) {
-                    //global.db.data.users[m.sender].banned = true
-                    if (m.fromMe) return
-                    m.reply('*Kamu dibanned karena spam!!!*')
-                    m.reply(`
-                    {
-                    status : banned
-                    { result : 404 }
-                    let users = global.db.data.users
-                    users[m.sender].banned = true
-                    }`)
-                    let users = global.db.data.users
-                    users[m.sender].banned = true
-                    //if (m.fromMe) return
-                //db.data.users[m.sender].banned = true
-                //db.data.users[m.sender].banned = true
-                    
-                }
-                this.spam[m.sender].count = 0
-                this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
-            }
-        }
-        else this.spam[m.sender] = {
-            jid: m.sender,
-            count: 0,
-            lastspam: 0
-        }
-    }
-}
-
-/*
-
-handler.all = async function (m) {
-    //if (db.data.settings[this.user.jid].antispam) return // antispam aktif?
-    if (m.isBaileys && m.fromMe) return
-    if (!m.message) return
-    if (!m.isCommand) return
-    if (db.data.users[m.sender].banned) return
-    if (db.data.chats[m.chat].isBanned) return
+export async function all(m) {
+    if (!m.message)
+        return
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
-        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
-            if (this.spam[m.sender].count > 3) {
-                
-                await this.sendButton(m.chat, 'kamu dibanned karena spam!', footer, 'Pemilik Bot', '.nowner')
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 5) {
+            if (this.spam[m.sender].count > 5) {
+                global.db.data.users[m.sender].banned = true
+                m.reply('*📮Kamu di banned karena spam*\n\n*💬Laporkan masalah ini ke wa.me/6281257172080?text=📧Bang+tolong+unban+nomor+ku*')
             }
             this.spam[m.sender].count = 0
             this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
         }
     }
-    else this.spam[m.sender] = {
-        jid: m.sender,
-        count: 0,
-        lastspam: 0
-    }
+    else
+        this.spam[m.sender] = {
+            jid: m.sender,
+            count: 0,
+            lastspam: 0
+        }
 }
-
-module.exports = handler
-*/
