e47aed4441e4a6f13bf5311b4cbdf077fd108aeb
ibengar <56824876+ibengar@users.noreply.github.com>
2022-09-04 06:04:50 +0800

    Update fitnah.js

--- plugins/fitnah.js
+++ plugins/fitnah.js
@@ -1,28 +1,26 @@
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text }) => {
+	let user = global.db.data.users[m.sender]
+	if (user.acc == true) {
   if (!text) return
   let cm = copy(m)
   let who
-  if (m.isGroup) who = cm.participant = m.mentionedJid[0]
+  if (text.includes('@0')) who = '0@s.whatsapp.net'
+  else if (m.isGroup) who = cm.participant = m.mentionedJid[0]
   else who = m.chat
-  if (!who) throw 'Tag orang yang mau dfitnah!\nMisal: !fitnah gw gay @user Ngaku'
+  if (!who) throw 'Tag salah satu lah'
   cm.key.fromMe = false
   cm.message[m.mtype] = copy(m.msg)
   let sp = '@' + who.split`@`[0]
   let [fake, ...real] = text.split(sp)
-  conn.fakeReply(m.chat, real.join(sp).trimStart(), who, fake.trimEnd()/*, { contextInfo: {
-    mentionedJid: [conn.parseMention(real.join(sp))]
-  }}*/)
-}
+  conn.fakeReply(m.chat, real.join(sp).trimStart(), who, fake.trimEnd(), m.isGroup ? m.chat : false, {
+    contextInfo: {
+      mentionedJid: conn.parseMention(real.join(sp).trim())
+    }
+  })
+}}
 handler.command = /^(fitnah|fakereply)$/
-handler.help = ['fitnah <teks> @user <teks>']
-handler.tags = ['group']
-handler.owner = false
-handler.mods = false
 handler.premium = true
-handler.group = true
-handler.private = false
-
 module.exports = handler
 
 function copy(obj) {
