05dd60d59f44656324d2ee0b8382175c8bf39209
ibengar <56824876+ibengar@users.noreply.github.com>
2022-09-04 06:22:17 +0800

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,12 +1,26 @@
-import { instagramdl, instagramdlv2, } from '@bochilteam/scraper'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
-    const results = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0]))                
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `ðŸ”— *Url:* ${url}`, m)
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let url = args[0]
+  let res = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=31d6c747fd7bd984fb6f7d95&url=${args[0]}`) 
+  if (res.status !== 200) {
+    res.text()
+    throw res.status
+  }
+  let json = await res.json()
+  if (!json.result) throw json
+  let { name, username, likes, caption, data } = json.result
+  let text = `
+Username: ${name} *(@${username})*
+${likes} Likes
+Caption:
+${caption}
+`.trim()
+    conn.sendFile(m.chat, url, 'ig' + (type == 'video' ? '.mp4' : '.jpg'), text, m)
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^(ig(dl)?)$/i
 
-export default handler
+module.exports = handler
