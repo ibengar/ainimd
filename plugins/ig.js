b32610164073dd64f7cb286c46e0cc9078233461
ibengar <56824876+ibengar@users.noreply.github.com>
2022-09-05 01:47:42 +0800

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,69 +1,15 @@
-let { instagramdlv3, instagramdlv4 } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-    if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-    if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-    const results = await instagramdlv3(args[0]).catch(async _ => await instagramdlv4(args[0]))
-    for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${await shortlink(url)}\n*${global.wm}*`, m)
+import instagramGetUrl from 'instagram-url-direct'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+    if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
+    let res = await instagramGetUrl(args[0])
+    for (let result of res.url_list) {
+    conn.sendFile(m.chat, result, 'instagram.mp4', author, m)
+  }
 }
-
-handler.help = ['ig'].map(v => v + ' <url>')
+handler.help = ['instagram <url>']
 handler.tags = ['downloader']
-handler.command = /^(ig|igdl|instagram)$/i
+handler.command = /^i(nsta)?g(ram)?(dl)?$/i
+
 handler.limit = true
-handler.group = true
 
-module.exports = handler
-
-async function shortlink(url) {
-isurl = /https?:\/\//.test(url)
-return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
-}
-
-
-/*const { instagramdl } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
-
-instagramdl(args[0]).then(async res => {
-    let instagramdl = JSON.stringify(res)
-    let json = JSON.parse(instagramdl)
-    for (let { url, type } of json) {
-      await delay(1500)
-      conn.sendFile(m.chat, url, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), 'ArieTube', m, { thumbnail: Buffer.alloc(0) })
-    }
-  })
-}
-handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(ig|igdl|instagram)$/i
-handler.limit = true
-handler.premium = false
-
-module.exports = handler
-
-const delay = time => new Promise(res => setTimeout(res, time))*/
-
-
-/*
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-  if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
-  let res = await fetch(`https://x-restapi.herokuapp.com/api/ig-dl?url=${args[0]}&apikey=BETA`)
-  let json = await res.json()
-   if (!json.status) throw json
-  m.reply('Sedang diproses...')
-   conn.sendFile(m.chat, json.linkdownload, 'ig.mp4', '*© ArieTube*', m, { thumbnail: Buffer.alloc(0) })
-}
-
-handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(ig|igdl|instagram)$/i
-handler.limit = true
-handler.group = false
-
-module.exports = handler
-
-const delay = time => new Promise(res => setTimeout(res, time))*/
+export default handler
