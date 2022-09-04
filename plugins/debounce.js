ab7e89ca000e523a7ffc3ff3e65d63e737bb8289
ibengar <56824876+ibengar@users.noreply.github.com>
2022-09-04 16:58:47 +0800

    Add files via upload

--- plugins/debounce.js
+++ plugins/debounce.js
@@ -8,7 +8,7 @@ let handler  = async (m, { conn }) => {
   } else throw '_eeeeeiiittsssss..._'
 }
 handler.help = ['debounce' + (process.send ? '' : ' (Not working)')]
-handler.tags = ['owner']
+handler.tags = ['host']
 handler.command = /^debounce$/i
 handler.owner = true
 handler.mods = false
