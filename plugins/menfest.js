627ce627763f6fb49c475fe018d67e832c0b2e78
ibengar <56824876+ibengar@users.noreply.github.com>
2022-09-05 00:00:23 +0800

    Update menfest.js

--- plugins/menfest.js
+++ plugins/menfest.js
@@ -1,31 +1,29 @@
-// Fitur By Zivsan And Haori Kasih Credits Dong Stah
-const { MessageType } = require('@adiwajshing/baileys')
-let fetch = require('node-fetch')
-let handler = async(m, { conn, text }) => {
-let [number, pesan] = text.split `|`
+import pkg from '@adiwajshing/baileys'
+import fetch from 'node-fetch'
 
-    if (!number) return conn.reply(m.chat, 'Penggunaan .chat nomor|pesan untuknya\nContoh : .chat 628xxxxxxxxxx|hai owner', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/b9a32ee41970d7a71b476.jpg')).buffer(),
-        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/6285246423078?text=.chat 6281257172080|Hi Jarot Hehehe 🌹', body: 'ibeng By ibeng', thumbnail: await (await fetch('https://telegra.ph/file/345dfdd724db22d617ed3.jpg')).buffer(),}}})
-    if (!pesan) return conn.reply(m.chat, 'Silahkan masukan pesannya', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/b9a32ee41970d7a71b476.jpg')).buffer(),
-        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/6285246423078?text=.chat 6281257172080|Hi Jarot Hehehe 🌹', body: 'ibeng By ibeng', thumbnail: await (await fetch('https://telegra.ph/file/345dfdd724db22d617ed3.jpg')).buffer(),}}})
-    if (text > 700) return conn.reply(m.chat, 'Teks Kepanjangan!', m, {quoted: m, thumbnail: await (await fetch('https://telegra.ph/file/b9a32ee41970d7a71b476.jpg')).buffer(),
-        contextInfo: { forwardingScore: 999, isForwarded: true, externalAdReply: {title: 'Gunakan Dengan Bijak Yah Kak', sourceUrl: 'http://wa.me/6285246423078?text=.chat 6281257172080|Hi Jarot Hehehe 🌹', body: 'ibeng By ibeng', thumbnail: await (await fetch('https://telegra.ph/file/345dfdd724db22d617ed3.jpg')).buffer(),}}})
+const { MessageType } = pkg
 
-    let chat = `${number}`
-    var nomor = m.sender
-    let chat1 = `Hi Saya Bot Ada Yang Kirim Pesan Ke Kamu
+let handler = async(m, { conn, args, usedPrefix }) => {
+	if (!args[0] || !args[1]) throw `Penggunaan .chat nomor|pesan untuknya\nContoh : .chat 628xxxxxxxxxx|hai owner`
+	let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : args[0] ? (args[0].replace(/[@ .+-]/g, '') + '@s.whatsapp.net') : ''
+	if (!who) `Penggunaan .chat nomor|pesan untuknya\nContoh : .chat 628xxxxxxxxxx|hai owner`
+	args[1] = args[1].join(' ')
+	if (args[1] > 700) throw `Teks Kepanjangan!`
+
+	var nomor = m.sender
+	let chat1 = `Hi Saya Bot Ada Yang Kirim Pesan Ke Kamu
 Seseorang Temanmu
 (Pengirim Rahasia)
 
 ⬡──⬡─────────⬡──⬡
 
-💌 Pesan : ${pesan}
+💌 Pesan : ${args[1]}
 
 ⬡──⬡─────────⬡──⬡
 
 Maaf Anda Belum Bisa Membalas ke Pengirim`
 
-let jarot = `▮PENGIRIM RAHASIA 」 
+let cap = `▮PENGIRIM RAHASIA 」 
 Anda Ingin Mengirimkan Pesan ke pacar/sahabat/teman/doi/
 mantan?, tapi Tidak ingin tau siapa Pengirimnya?
 Kamu bisa menggunakan Bot ini
@@ -33,10 +31,14 @@ Contoh Penggunaan: .chat nomor|pesan untuknya
 
 Contoh: #chat 628xxxxxxxxxx|hai owner`
 
-    conn.send2ButtonImg(chat + '@s.whatsapp.net', await(await fetch('https://telegra.ph/file/aa06b2e4067f6a6387b75.jpg')).buffer(), chat1, jarot, 'Menu', '.menu', '', '', m)
-    
-    let logs = `BOT AKAN BLOKIR KONTAKMU?
-    
+	//conn.send2ButtonImg(chat + '@s.whatsapp.net', await(await fetch('https://telegra.ph/file/aa06b2e4067f6a6387b75.jpg')).buffer(), chat1, cap, 'Menu', '.menu', 'Donasi', '.donasi', m)
+	conn.sendButton(who, chat1, cap, await(await fetch('https://telegra.ph/file/aa06b2e4067f6a6387b75.jpg')).buffer(), [
+		[`Menu`, `${usedPrefix}menu`],
+		[`Premium`, `${usedPrefix}prem`]
+	], m)
+	
+	let logs = `BOT AKAN BLOKIR KONTAKMU?
+	
 ≫ Spam
 ≫ Chat Aneh Aneh
 ≫ Berantem`
@@ -46,23 +48,18 @@ Contoh: #chat 628xxxxxxxxxx|hai owner`
 
 ⬡──⬡─────────⬡──⬡
 
-Isi Pesan : ${pesan}
+Isi Pesan : ${args[1]}
 
 ⬡──⬡─────────⬡──⬡`
-    conn.send2ButtonImg(m.chat, await (await fetch('https://telegra.ph/file/9ceda8d2274e97bad5bca.jpg')).buffer(), logs, haori1, 'Owner', 'owner', '', '', m)
+	//conn.send2ButtonImg(m.chat, await (await fetch('https://telegra.ph/file/9ceda8d2274e97bad5bca.jpg')).buffer(), logs, haori1, 'Owner', 'owner', 'Donasi', '.donasi', m)
+	conn.sendButton(m.chat, haori1, logs, await (await fetch('https://telegra.ph/file/9ceda8d2274e97bad5bca.jpg')).buffer(), [
+		[`Menu`, `${usedPrefix}menu`],
+		[`Premium`, `${usedPrefix}prem`]
+	], m)
 }
+
 handler.help = ['menfess <nomor|pesan>']
-handler.tags = ['main']
-handler.command = /^(menfess)$/i
-handler.owner = false
-handler.premium = true
-handler.group = false
-handler.private = false
+handler.tags = ['entertainment']
+handler.command = /^(menfess|chat)$/i
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.limit = false
-
-module.exports = handler
+export default handler
