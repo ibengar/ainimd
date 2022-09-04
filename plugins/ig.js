ec7d400505e2c4b26a2ec972f44075cd3fa15502
ibengar <56824876+ibengar@users.noreply.github.com>
2022-09-05 01:13:26 +0800

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,9 +1,9 @@
-let { instagramdl, instagramdlv2 } = require('@bochilteam/scraper')
+let { instagramdlv3, instagramdlv4 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
     if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
     if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
-    const results = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0]))
+    const results = await instagramdlv3(args[0]).catch(async _ => await instagramdlv4(args[0]))
     for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${await shortlink(url)}\n*${global.wm}*`, m)
 }
 
@@ -31,7 +31,7 @@ instagramdl(args[0]).then(async res => {
     let json = JSON.parse(instagramdl)
     for (let { url, type } of json) {
       await delay(1500)
-      conn.sendFile(m.chat, url, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), 'ArieTube', m, { thumbnail: Buffer.alloc(0) })
+      conn.sendFile(m.chat, url, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), 'Aine', m, { thumbnail: Buffer.alloc(0) })
     }
   })
 }
@@ -55,14 +55,14 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   let json = await res.json()
    if (!json.status) throw json
   m.reply('Sedang diproses...')
-   conn.sendFile(m.chat, json.linkdownload, 'ig.mp4', '*© ArieTube*', m, { thumbnail: Buffer.alloc(0) })
+   conn.sendFile(m.chat, json.linkdownload, 'ig.mp4', '*© Aine*', m, { thumbnail: Buffer.alloc(0) })
 }
 
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(ig|igdl|instagram)$/i
 handler.limit = true
-handler.group = false
+handler.group = true
 
 module.exports = handler
 
