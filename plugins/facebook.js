07a7c64581e0d36d7cc754fc14ab12b064e4055c
ibengar <56824876+ibengar@users.noreply.github.com>
2022-09-14 01:49:31 +0800

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -1,7 +1,7 @@
-const { facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
+const { facebookdlv2 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://fb.watch/azFEBmFRcy/`
-    const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+    const { result } =  await facebookdlv2(args[0]))
     for (const { url, isVideo } of result.reverse()) conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `ðŸ”— *Url:* ${url}`, m)
 }
 handler.help = ['fb'].map(v => v + ' <url>')
